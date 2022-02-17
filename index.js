e4ec43abf2837821d8c79c0071b12001782c2487
Jimi Wen <jimi.wen@gmail.com>
2022-02-17 13:18:06 +0800

    mint

--- index.js
+++ index.js
@@ -1,130 +1,33 @@
-let fieldbend=Xrandom(0.2,1);
+let bgpicker= Xrandom(0,1);
+let boxStyle=Xrandom(0,1);
 
-
-// console.log(fieldbend)
-
-let centerpoint=[Xrandom(300,800),Xrandom(500,700)];
-let sizeratio=[Xrandom(100,200),200];
-let depth=[Xrandom(30,100),Xrandom(40,130)]
-let right_vanish= Xrandom(0,100);
-let lower_ceiling=Xrandom(150,150);
-let whitepaint=-Xrandom(40,80);
-let topfaceangle= Xrandom(50,150);
-let leftceilingangle= Xrandom(50,150);
-let colorpicker= Xrandom(0,1);
-let rotateStyle=Xrandom(0,1);
 let leadCames=Xrandom(2.1,5.1);
-//
-// console.log(rotateStyle)
-// console.log(getColorStyle(colorpicker))
-// console.log(getRotateStyle(rotateStyle))
-// console.log(getPerspectiveNearFar(centerpoint[0],centerpoint[1],rotateStyle))
 
 function Xrandom(x,y){
   return (y-x)*fxrand()+x;
 }
 
 window.$fxhashFeatures = {
-  "Color Palette": getColorStyle(colorpicker),
-  "Ceremony Act": getPerspectiveNearFar(centerpoint[0],centerpoint[1],rotateStyle),
-  "Right Side Up?": getRotateStyle(rotateStyle),
+  "YOUR WORLD BLEND": getBGStyle(bgpicker),
+  "tea hut blend": getBoxStyle(boxStyle),
 }
 
-function getPerspectiveNearFar(x,y,rotateStyle){
-  if ( rotateStyle<0.8 &&  x <600 && y<600) return "Entering the tea house"
-  else if (rotateStyle<0.8 && x<500) return "Sitting and contemplating"
-  else if ( rotateStyle<0.8 &&  x>600) return "Observing the making of tea"
-  else if (rotateStyle<0.8) return "Appreciation of tea"
-  else return "Not Applicable"
+console.log(getBoxStyle(boxStyle))
+console.log(getBGStyle(bgpicker))
+
+
+function getBGStyle(value){
+  if (value<0.05) return 'SCREEN';
+  else if (value<0.15) return 'ADD'
+  else if (value<0.45) return 'DIFFERENCE'
+  else return 'OVERLAY'
 }
 
-function getColorStyle(value){
-  if (value<0.15) return 'Day';
-  else if (value<0.25) return 'Night'
-  else if (value<0.33) return 'Anxious Purple'
-  else if (value<0.40) return 'Bling'
-  else if (value<0.65) return 'Pastel'
-  else if (value<0.85) return 'Negative'
-  else return 'Dark Riyku'
+function getBoxStyle(value){
+  if (value<0.1) return 'ADD';
+  else if (value<0.3) return 'OVERLAY'
+  else return 'LIGHTEST'
 }
-
-function getRotateStyle(value){
-  if (value<0.8) return 'Tea Time';
-  else if (value<0.87) return 'Good Night'
-  else if (value<0.94) return 'Bath Time'
-  else return 'Upside-down'
-}
-
-if (colorpicker < 0.15 ){
-  // gucci
-  colors = {
-    main: "#d9d2ce",
-    bg: "#120b17",
-    roof:'#b03737',
-    body2:'#234031'
-  };
-} else if (colorpicker< 0.25 ){
-  //  dark gucci
-  colors = {
-    main: "#0f0f12",
-    bg: "#0f0f12",
-    roof:'#b03737',
-    body2:'#234031'
-  };
-} else if (colorpicker< 0.33 ){
-  //  lakers inverted
-  colors = {
-    main: "#f5c207",
-    bg: "#140621",
-    roof:'#140621',
-    body2:'#140621'
-  };
-} else if (colorpicker < 0.40){
-  //gold
-  colors = {
-    main: "#b03737",
-    bg: "#09060d",
-    roof:'#ffd608',
-    body2:'#f5c207'
-  };
-}else if (colorpicker < 0.65){
-  //pastel blue
-  colors = {
-    main: "#061521",
-    bg: "#061521",
-    roof:'#96949c',
-    body2:'#524c57'
-  };
-}else if (colorpicker < 0.85){
-  //film negative
-  colors = {
-    bg: "#2e2e2e",
-    main: "#2e2e2e",
-    roof:'#6e6e6e',
-    body2:'#cccccc'
-  };
-}else{
-  //dark rikyu
-  colors = {
-    bg: "#2e2e2e",
-    main: "#2e2e2e",
-    roof:'#949079',
-    body2:'#5e5246'
-  };
-}
-
-
-//
-//
-//  colors = {
-//   main: "#d9d2ce",
-//   bg: "#120b17",
-//   roof:'#b03737',
-//   body2:'#234031'
-// };
-
-
-
 
 
 function setup() {
@@ -167,11 +70,11 @@ function setup() {
 
   coloring3={
     color1:'#234059',
-    color2:'#b85182',
-    color3:'#234759',
-    color4:'#404239',
+    color2:'#eb5282',
+    color3:'#534759',
+    color4:'#b0b877',
     color5:'#424059',
-    color6:'#983059'
+    color6:'#369ad9'
   }
 }
 
@@ -201,17 +104,28 @@ function draw() {
 
   backgroundlucent=255
   background(random(0,255),random(0,255),random(0,255),backgroundlucent)
-  //  blendMode(ADD)//overexposed background
-  //blendMode(DIFFERENCE) //complex background
+  // blendMode(ADD)//overexposed background
+
   //blendMode(MULTIPLY) //too green
-  //blendMode(SCREEN) //Lighter
-  blendMode(OVERLAY) //ok maybe
   //blendMode(HARD_LIGHT )//ok maybe
   //blendMode(LIGHTEST) // no good details gone
   //do nothing stark contrast
   //blendMode(DODGE) //bright contrast exposed sometime details gone
   //blendMode(BURN)
   //blendMode(DARKEST ) //no good??
+
+if (bgpicker<0.05){
+    blendMode(SCREEN) //Lighter
+}else if (bgpicker<0.15){
+  blendMode(ADD)//overexposed background
+}else if (bgpicker<0.45){
+  blendMode(DIFFERENCE) //complex background
+}else{
+  blendMode(OVERLAY) //ok maybe
+}
+
+
+
 
   fill(random(0,255),random(0,255),random(0,255))
   //ground
@@ -237,18 +151,28 @@ function draw() {
   drawstonesteps(c)
   rect(1200,300,random(300,600),1000+random(100,900))
 
-
+if (boxStyle<0.1){
+  blendMode(ADD)//jumps out negatie frame
+  uvfilter=random(200,200)
+}else if (boxStyle<0.3){
+  blendMode(OVERLAY)//jumpsout dark frame
+  uvfilter=random(250,250)
+}else{
+  blendMode(LIGHTEST) //standard
+  uvfilter=random(200,200)
+}
 
 
 
   // let	x = 600+random(-200,200)
   // let y = 600+random(-200,200)
   //
-  blendMode(LIGHTEST)
-  uvfilter=random(200,200)
-  //blendMode(ADD)//jumps out negatie frame
-  //blendMode(OVERLAY)//jumpsout dark frame
-  //uvfilter=random(250,250)
+
+
+
+
+
+
 
 
   reflection=random(-0.1,0.1)
@@ -264,7 +188,7 @@ function draw() {
 }
 
 function exportImage() {
-  save(`void_${fxhash}.png`)
+  save(`433_${fxhash}.png`)
 }
 
 
@@ -475,6 +399,7 @@ function carve3(uvfilter,vasewidth,flower,leadCames){
     carve2(position[0],position[1],8,0)
     carve2(position[0],position[1],4,0)
     carve2(position[0],position[1],2,0)
+    carve2(position[0],position[1],1,0)
 
 
     position=carve2(650+cos(30)*150+cos(30)*20,530-sin(30)*150+sin(30)*20,16,0)
@@ -482,29 +407,22 @@ function carve3(uvfilter,vasewidth,flower,leadCames){
     carve2(position[0],position[1],8,0)
     carve2(position[0],position[1],4,0)
     carve2(position[0],position[1],2,0)
+    carve2(position[0],position[1],1,0)
 
     position=carve2(486+cos(30)*20,430-sin(30)*20,16,0)
     carve2(position[0],position[1],8,0)
     carve2(position[0],position[1],4,0)
     carve2(position[0],position[1],2,0)
+    carve2(position[0],position[1],1,0)
 
     position=carve2(600+cos(30)*150,600-sin(30)*150,16,0)
     //console.log(position)
     carve2(position[0],position[1],8,0)
     carve2(position[0],position[1],4,0)
     carve2(position[0],position[1],2,0)
+    carve2(position[0],position[1],1,0)
 
 
-    position=carve2(650+cos(30)*150+cos(30)*20,530-sin(30)*150+sin(30)*20,16,0)
-    //console.log(position)
-    carve2(position[0],position[1],8,0)
-    carve2(position[0],position[1],4,0)
-    carve2(position[0],position[1],2,0)
-
-    position=carve2(486+cos(30)*20,430-sin(30)*20,16,0)
-    carve2(position[0],position[1],8,0)
-    carve2(position[0],position[1],4,0)
-    carve2(position[0],position[1],2,0)
 
   }else{
 
@@ -532,18 +450,8 @@ function carve3(uvfilter,vasewidth,flower,leadCames){
   y3=[600+cos(30)*149,600-sin(30)*149];
   y6=[600+cos(30)*0,600-sin(30)*0];
   y7=[600+cos(30)*149,600+sin(30)*149];
-  strokeWeight(100)
-  connectlines(x1)
-  connectlines(x2)
-  connectlines(x3)
-  connectlines(x4)
-  connectlines(y1)
-  connectlines(y2)
-  connectlines(y3)
-  connectlines(y4)
-  connectlines(y5)
-  connectlines(y6)
-  connectlines(y7)
+
+
 
 
   // beginShape()
@@ -673,7 +581,7 @@ function carve3(uvfilter,vasewidth,flower,leadCames){
   endShape(CLOSE)
 
   vasewidth=random([5,10])
-  boxsize=random(100,400);
+  boxsize=random(480,500);
   o1=random([-100,0,0,0,0,100]);
   o2=random([-100,0,0,0,0,100]);
   o3=random([-100,0,0,0,0,100]);
@@ -701,13 +609,6 @@ function carve3(uvfilter,vasewidth,flower,leadCames){
 
 }
 
-function connectlines(pos){
-
-  line(pos[0],pos[1],0,0)
-  line(pos[0],pos[1],0,1200)
-  line(pos[0],pos[1],1200,0)
-  line(pos[0],pos[1],1200,1200)
-}
 
 function hexToRgb(hex) {
   hex = hex.replace('#', '');
@@ -927,5 +828,5 @@ return position;
 }
 
 //     License : Copyright (C) 2022 Jimi Y. C. Wen . All rights reserved.\n
-//     Licensed under CC BY-NC-SA 4.0 See LICENSE
+//     Licensed under CC BY-NC-SA 4.0
 //    https://github.com/jimiwen
